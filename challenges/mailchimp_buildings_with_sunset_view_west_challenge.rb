b0de34557955f2739bf9967532f29274df2255fe
Renan Medina <renan@silvamedina.com.br>
2024-06-11 15:04:19 -0300

    chore: Adding mailchimp buildings with sunsetview challenge

---
+++ challenges/mailchimp_buildings_with_sunset_view_west_challenge.rb
@@ -0,0 +1,39 @@
+=begin
+Good morning! Here's your coding interview problem for today.
+This problem was asked by Mailchimp.
+You are given an array representing the heights of neighboring buildings on a city street, from east to west. 
+The city assessor would like you to write an algorithm that returns how many of these buildings have a view of the setting sun, 
+in order to properly value the street.
+
+For example, given the array [3, 7, 8, 3, 6, 1], you should return 3, since the top floors of the buildings with heights 8, 6, and 1 
+all have an unobstructed view to the west.
+Can you do this using just one forward pass through the array?
+=end
+
+def buildings_with_sunset_view(buildings:)
+  i = buildings.count - 1
+  biggest_height = buildings.last
+  solution_buildings = [biggest_height]
+
+  while i >= 0
+    building_height = buildings[i]
+    solution_buildings << building_height if building_height > biggest_height
+    biggest_height = building_height if building_height > biggest_height
+    i -= 1
+  end
+
+  solution_buildings
+end
+
+samples = [
+  [3, 7, 8, 3, 6, 1],
+  [4, 9, 15, 10, 12],
+  [1, 2, 3, 4, 5, 6],
+  [25, 13, 32, 10, 8],
+  [65, 13, 32, 10, 8],
+]
+
+samples.each do |sample|
+  solution = buildings_with_sunset_view(buildings: sample)
+  puts "The buildings with sunset view from [#{sample.join(', ')}] are: [#{solution.join(', ')}] (count: #{solution.count})"
+end
