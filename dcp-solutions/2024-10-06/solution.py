9f88cf35932eb7371f59c8b94163a4af26abb88a
dcp-solver <dcp-solver@silvamedina.com.br>
2024-12-28 18:27:49 -0300

    Adding file at dcp-solutions/2024-10-06/solution.py

---
+++ dcp-solutions/2024-10-06/solution.py
@@ -0,0 +1,13 @@
+```python
+def are_brackets_balanced(s: str) -> bool:
+    stack = []
+    mapping = {")": "(", "}": "{", "]": "["}
+
+    for char in s:
+        if char in mapping.values():
+            stack.append(char)
+        elif char in mapping.keys():
+            if not stack or mapping[char] != stack.pop():
+                return False
+    return not stack
+```
